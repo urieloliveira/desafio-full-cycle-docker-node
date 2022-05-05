9956339e46456dd09764afee839ce0ba99215c04
Uriel Guy <urielsateles@gmail.com>
2022-05-05 20:53:49 -0300

    feat: list names

--- index.js
+++ index.js
@@ -9,13 +9,21 @@ const config = {
 };
 const mysql = require("mysql");
 const connection = mysql.createConnection(config);
+const sql = `INSERT INTO people(name) values('Uriel');`;
+const select = `SELECT * FROM people`;
+let list = "";
 
-const sql = `INSERT INTO people(name) values('Wesley')`;
 connection.query(sql);
+connection.query(select, (err, results) => {
+  if (err) throw err;
+  list = `<ul>${results
+    .map((person) => `<li>${person.name}</li>`)
+    .join("")}</ul>`;
+});
 connection.end();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Full Cycle Rocks!</h1>");
+  res.send(`<h1>Full Cycle Rocks!</h1>${list}`);
 });
 
 app.listen(port, () => {
